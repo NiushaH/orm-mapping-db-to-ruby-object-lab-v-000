e0df11b58cfe76ed068180c59ddf90639b7e81cf
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-12 06:10:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    @student = Student.new
+    new_student = Student.new
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
