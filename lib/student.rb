bb1780c4d2d4839608843c08d54750d1283d689b
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-12 06:10:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,20 +1,6 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
-  end
-
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-  end
-
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-  end
-  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
@@ -40,4 +26,68 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+# new code here
+  
+  
+  def self.new_from_db(row)
+  end
+
+  def self.all
+  end
+
+  def self.find_by_name(name)
+  end
+  
+  
+  
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    @student = Student.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+  
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+  end
+  
+    def self.find_by_name(student_name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    self.all.select { |s| s.name.include?(student_name)}
+  end
+  
+  
+  def self.all_students_in_grade_9
+  
+  end
+  
+  def self.students_below_12th_grade
+  
+  end
+  
+  def self.first_X_students_in_grade_10(total_gr10_students)
+  
+  end
+  
+  def self.first_student_in_grade_10
+  
+  end
+  
+  def self.all_students_in_grade_X(grade)
+  
+  end 
+end
+  
 end
